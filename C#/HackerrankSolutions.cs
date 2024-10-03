a48e9e5d02bea89ef8ae55c39952efef139f2baf
1714546 <harveysutton09@gmail.com>
2024-10-03 21:49:05 +0100

    C# hackerrank setup
    first commit for c# solutions

---
+++ C#/HackerrankSolutions.cs
@@ -0,0 +1,23 @@
+using System;
+
+class HackerRankSolutions
+{
+    // Sum of an array of integers
+    public static int simpleArraySum(List<int> ar)
+    {
+        int sum = 0;
+        foreach (int num in ar)
+        {
+            sum += num;
+        }
+        return sum;
+    }
+    
+    
+    // Main method to test the solutions
+    public static void Main()
+    {
+        // Call specific methods to test the solutions
+        
+    }
+}
